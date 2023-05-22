b4191996c42c51ae1dc47fe6afc1d1147109e5e1
dpatterson8583 <dpatterson8583@gmail.com>
2023-05-22 13:06:19 -0500

    New commit for additional test data.

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -7,25 +7,35 @@ namespace UnitTestingExercise.Tests
     {
         [Theory]
         [InlineData(2, 3, 5, 10)] //Add test data <-------
+        [InlineData(1, 2, 5, 8)]
+        [InlineData(2, 4, 5, 11)]
+        [InlineData(3, 5, 5, 13)]
+        [InlineData(4, 3, 5, 12)]
+        [InlineData(5, 3, -5, 3)]
         public void AddTest(int num1, int num2, int num3, int expected)
         {
             //Start Step 3 here:
 
             //Arrange
-            // create a Calculator object
+            // DONE: create a Calculator object
             var calc = new Calculator();
 
             //Act
-                // call the Add method that is located in the Calculator class
-                // and store its result in a variable named actual
-                var actual = calc.Add(num1, num2, num3);
+            // DONE: call the Add method that is located in the Calculator class
+            // DONE:and store its result in a variable named actual
+            var actual = calc.Add(num1, num2, num3);
 
             //Assert
                 Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData(10,5,5)]//Add test data <-------
+        [InlineData(10,5,5)]//DONE:  Add test data <-------
+        [InlineData(20, 5,15)]
+        [InlineData(30, 15, 15)]
+        [InlineData(40, 20, 20)]
+        [InlineData(50, -5, 55)]
+        [InlineData(60, 35, 25)]
         public void SubtractTest(int minuend, int subtrhend, int expected)
         {
             //Start Step 5 here:
@@ -42,7 +52,11 @@ namespace UnitTestingExercise.Tests
         }
 
         [Theory]
-        [InlineData(3,5,15)]//Add test data <-------
+        [InlineData(3,5,15)]//DONE:Add test data <-------
+        [InlineData(7, 5, 35)]
+        [InlineData(7, 3, 21)]
+        [InlineData(12, 11, 132)]
+        [InlineData(0, 5, 0)]
         public void MultiplyTest(int num1, int num2, int expected)
         {
             //Start Step 7 here:
@@ -59,7 +73,11 @@ namespace UnitTestingExercise.Tests
         }
 
         [Theory]
-        [InlineData(6,2,3)]//Add test data <-------
+        [InlineData(6,2,3)]//DONE:  Add test data <-------
+        [InlineData(16, 8, 2)]
+        [InlineData(27, 3, 9)]
+        [InlineData(-6, 2, -3)]
+        [InlineData(6, -2, -3)]
         public void DivideTest(int num1, int num2, int expected)
         {
             //Arrange
